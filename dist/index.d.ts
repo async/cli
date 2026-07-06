7ea228b9b474249e40b70d0555efb1953b67302b
PatrickJS <github@patrickjs.com>
2026-07-06 11:26:26 -0700

    fix(api): export command result types

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,7 +2,7 @@ export { packageInfo } from "./package-info.js";
 export type { AsyncCliPackageInfo } from "./package-info.js";
 export { CliError, availableTemplates, copyCommand, createCommand, discoverRoots, executeResolution, findRunnableScript, listCommands, moveCommand, readCwdPragma, removeCommand, resolveCommand, resolveScopedRoot, resolveScriptCwd } from "./router.js";
 export { runCommand } from "./run.js";
-export type { CliErrorCode, CommandEntry, CommandResolution, CommandRoot, CopyCommandOptions, CopyCommandResult, CreateCommandOptions, DiscoverRootsOptions, ListCommandsOptions, MoveCommandOptions, RemoveCommandOptions, RemoveCommandResult, ResolveCommandOptions, RunCommandOptions, ScriptCwdMode } from "./router.js";
+export type { CliErrorCode, CommandEntry, CommandList, CommandResolution, CommandRoot, CopyCommandOptions, CopyCommandResult, CreateCommandOptions, CreateCommandResult, DiscoverRootsOptions, ListCommandsOptions, MoveCommandOptions, MoveCommandResult, RemoveCommandOptions, RemoveCommandResult, ResolveCommandOptions, RunCommandOptions, ScriptCwdMode } from "./router.js";
 export { ensureOverlayTrusted, hashOverlayTree, isTrustEnforced, localOverlayTrust, overlayTrustState, recordOverlayTrust, removeOverlayTrust, trustLocalOverlays, trustStorePath, untrustLocalOverlays } from "./trust.js";
 export type { OverlayTrust, OverlayTrustState } from "./trust.js";
 export { builtinFlags, complete, completionScript } from "./completions.js";
